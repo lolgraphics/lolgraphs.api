1e76742fae0b26bdd3acde041075c52c7cb1fe92
AkyrisPinange <akyris.2903@gmail.com>
2024-07-31 21:37:10 -0300

    tetetetet

---
+++ Lolgraphics.Core/Services/GreeterService .cs
@@ -0,0 +1,17 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class GreeterService : IGreeterService
+    {
+        public string Greet(string name)
+        {
+            return $"Hello, {name}!";
+        }
+    }
+}
---
+++ Lolgraphics.Tests/Service/GreeterServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class GreeterServiceTests
+    {
+
+        [Fact]
+        public void Greet_ReturnsCorrectGreeting()
+        {
+            // Arrange
+            var service = new GreeterService();
+            var name = "World";
+
+            // Act
+            var result = service.Greet(name);
+
+            // Assert
+            Assert.Equal("Hello, World!", result);
+        }
+    }
+}

ababc914847832a9455e425c0c1b5971bf76fff2
AkyrisPinange <akyris.2903@gmail.com>
2024-08-17 23:12:16 -0300

    Update MongoStartup.cs

--- Mongo/MongoStartup.cs
+++ Mongo/MongoStartup.cs
@@ -15,10 +15,10 @@ namespace Mongo
 
             services.Configure<MongoDbSettings>(options =>
             {
-                options.ConnectionString = "mongodb+srv://lolGraphics:xv343He7d9GFHBVb@lolgraphicsdev.ilwnyb4.mongodb.net/?retryWrites=true&w=majority&appName=lolGraphicsDev";
-                // ?? throw new InvalidOperationException("Mongo Connection string configuration is missing.");
-                options.DatabaseName = "graphics";
-                       // ?? throw new InvalidOperationException("Mongo database string is missing.");
+                options.ConnectionString = configuration["MONGODB_CONNECTIONSTRING"]
+                 ?? throw new InvalidOperationException("Mongo Connection string configuration is missing.");
+                options.DatabaseName = configuration["MONGODB_DATABASE"]
+                        ?? throw new InvalidOperationException("Mongo database string is missing.");
             });
 
             services.AddSingleton<IMongoClient>(sp =>
